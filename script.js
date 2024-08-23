ca4bf405a30283e45e6df37e038a9c035bb06d95
rod773 <rodrigoarcadio1@gmail.com>
2024-08-23 18:34:11 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //DOM elements
-const coverflowContainer = document.querySelector('.coverflow');
-const coverflowImages = [...document.querySelectorAll('.coverflow__image')];
-const prevArrow = document.querySelector('.prev-arrow');
-const nextArrow = document.querySelector('.next-arrow');
+const coverflowContainer = document.querySelector(".coverflow");
+const coverflowImages = [...document.querySelectorAll(".coverflow__image")];
+const prevArrow = document.querySelector(".prev-arrow");
+const nextArrow = document.querySelector(".next-arrow");
 
 //set indicies and initial position
-coverflowImages.forEach(function(coverflowImage, i) {
+coverflowImages.forEach(function (coverflowImage, i) {
   coverflowImage.dataset.coverflowIndex = i + 1;
 });
-let coverflowPosition = Math.floor(coverflowImages.length/2) + 1;
+let coverflowPosition = Math.floor(coverflowImages.length / 2) + 1;
 coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 
 //navigation functions
@@ -21,20 +21,32 @@ function viewNextImage() {
   coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 }
 function jumpToImage(targetImage) {
-  coverflowPosition = Math.min(coverflowImages.length, Math.max(1, targetImage.dataset.coverflowIndex));
+  coverflowPosition = Math.min(
+    coverflowImages.length,
+    Math.max(1, targetImage.dataset.coverflowIndex)
+  );
   coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 }
 
 //add event handlers
-prevArrow.addEventListener('click', viewPrevImage);
-nextArrow.addEventListener('click', viewNextImage);
-coverflowImages.forEach(function(image) {
-  image.addEventListener('click', evt => jumpToImage(evt.target));
+prevArrow.addEventListener("click", viewPrevImage);
+nextArrow.addEventListener("click", viewNextImage);
+coverflowImages.forEach(function (image) {
+  image.addEventListener("click", (evt) => jumpToImage(evt.target));
 });
-window.addEventListener('keyup', (evt) => {
-  if (evt.which ===  37) { //left arrow
+window.addEventListener("keyup", (evt) => {
+  if (evt.which === 37) {
+    //left arrow
     viewPrevImage();
-  } else if (evt.which === 39) { //right arrow
+  } else if (evt.which === 39) {
+    //right arrow
     viewNextImage();
   }
+});
+
+window.addEventListener("scroll", (event) => {
+  //console.log(this.oldScroll > this.scrollY);
+  if (this.oldScroll > this.scrollY) viewPrevImage();
+  else viewNextImage();
+  this.oldScroll = this.scrollY;
 });
