b9123ac620dab08ba1c048bd2baa83033261f4c2
rod773 <rodrigoarcadio1@gmail.com>
2024-08-24 10:16:57 +0200

    update

--- script.js
+++ script.js
@@ -4,27 +4,37 @@ const coverflowImages = [...document.querySelectorAll(".coverflow__image")];
 const prevArrow = document.querySelector(".prev-arrow");
 const nextArrow = document.querySelector(".next-arrow");
 
+let cont = 0;
+
 //set indicies and initial position
 coverflowImages.forEach(function (coverflowImage, i) {
   coverflowImage.dataset.coverflowIndex = i + 1;
 });
+
 let coverflowPosition = Math.floor(coverflowImages.length / 2) + 1;
+
 coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 
 //navigation functions
 function viewPrevImage() {
-  coverflowPosition = Math.max(1, coverflowPosition - 1);
+  cont--;
+  coverflowPosition = cont % coverflowImages.length;
+  console.log(coverflowPosition);
   coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 }
 function viewNextImage() {
-  coverflowPosition = Math.min(coverflowImages.length, coverflowPosition + 1);
+  cont++;
+  coverflowPosition = cont % coverflowImages.length;
+  console.log(coverflowPosition);
   coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 }
+
 function jumpToImage(targetImage) {
   coverflowPosition = Math.min(
     coverflowImages.length,
     Math.max(1, targetImage.dataset.coverflowIndex)
   );
+
   coverflowContainer.dataset.coverflowPosition = coverflowPosition;
 }
 
@@ -33,15 +43,6 @@ prevArrow.addEventListener("click", viewPrevImage);
 nextArrow.addEventListener("click", viewNextImage);
 coverflowImages.forEach(function (image) {
   image.addEventListener("click", (evt) => jumpToImage(evt.target));
-});
-window.addEventListener("keyup", (evt) => {
-  if (evt.which === 37) {
-    //left arrow
-    viewPrevImage();
-  } else if (evt.which === 39) {
-    //right arrow
-    viewNextImage();
-  }
 });
 
 window.addEventListener("scroll", (event) => {
@@ -52,3 +53,7 @@ window.addEventListener("scroll", (event) => {
 
   this.oldScroll = this.scrollY;
 });
+
+setInterval(() => {
+  nextArrow.click();
+}, 2000);
